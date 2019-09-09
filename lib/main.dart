eaa921383e43ef80b945d703f3fb3235dd744f63
ZiadEzat <meroziko@gmail.com>
2019-09-09 03:32:52 +0200

    Added Navigation Bar & Drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,18 @@
-import 
+import "package:flutter/material.dart";
+
+import 'Home/home.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Homepage(),
+    );
+  }
+}
